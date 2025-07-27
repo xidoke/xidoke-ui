c2e06332510291700dd64693e16ed5278511c5fe
Phạm Đình Đô <dopd@avadagroup.com>
2025-07-27 07:57:45 +0700

    chore: update .gitignore, pnpm-lock.yaml, and Turbo settings; remove unused docs app

---
+++ apps/docs/.eslintrc.cjs
@@ -0,0 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ["@xidoke/eslint-config/storybook.js"],
+};
--- apps/docs/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import { nextJsConfig } from "@xidoke/eslint-config/next-js";
-
-/** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
